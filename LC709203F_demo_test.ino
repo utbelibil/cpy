7676ad44fba348fa3824bdd732a61475204dbbe7
utbelibil <95613894+utbelibil@users.noreply.github.com>
2022-05-24 19:45:53 +0200

    Add files via upload

---
+++ LC709203F_demo_test.ino
@@ -0,0 +1,31 @@
+#include "Adafruit_LC709203F.h"
+
+Adafruit_LC709203F lc;
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  Serial.println("\nAdafruit LC709203F demo");
+
+  if (!lc.begin()) {
+    Serial.println(F("Couldnt find Adafruit LC709203F?\nMake sure a battery is plugged in!"));
+    while (1) delay(10);
+  }
+  Serial.println(F("Found LC709203F"));
+  Serial.print("Version: 0x"); Serial.println(lc.getICversion(), HEX);
+
+  lc.setThermistorB(3950);
+  Serial.print("Thermistor B = "); Serial.println(lc.getThermistorB());
+
+  lc.setPackSize(LC709203F_APA_500MAH);
+
+  lc.setAlarmVoltage(3.8);
+}
+
+void loop() {
+  Serial.print("Batt Voltage: "); Serial.println(lc.cellVoltage(), 3);
+  Serial.print("Batt Percent: "); Serial.println(lc.cellPercent(), 1);
+  Serial.print("Batt Temp: "); Serial.println(lc.getCellTemperature(), 1);
+
+  delay(20000);  // dont query too often!
+}
